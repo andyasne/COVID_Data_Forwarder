c11298a270e6a5ff7f3cc87287081f5f6bf578fb
Andinet A <andyasne@gmail.com>
2020-06-16 11:56:16 +0300

    Added Req Transform map config

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const transform = require("node-json-transform").transform;
 
 const options = {
   target: 'http://3.95.229.192:3000', // target host
-  changeOrigin: true, // needed for virtual hosted sites
-   ws: true, // proxy websockets
+  changeOrigin:"properties.HouseNo", // needed for virtual hosted sites
+   ws:"properties.HouseNo", // proxy websockets
   pathRewrite: {
     '^/api': '/users', // rewrite path
     '^/api/remove/path': '/path', // remove base path
@@ -18,18 +18,13 @@ const options = {
   onProxyReq: function onProxyReq(proxyReq, req, res) {
     if ( req.method == "POST" && req.body ) {
 
-    var map = {
-    item: {
-      modifiednmame: "name"}
-    };
-
-    const resultObj = transform(req.body, map);
+    const resultObj = transformJSON(req);
     const result = JSON.stringify(resultObj);
     console.log(result);
 
 
    // Update header
-   proxyReq.setHeader( 'content-type', 'application/json' );
+  proxyReq.setHeader( 'content-type', 'application/json' );
   proxyReq.setHeader( 'content-length', result.length );
 
    // Write out body changes to the proxyReq stream
@@ -50,3 +45,58 @@ const app = express();
 app.use(express.json());
 app.use('/api', covidProxy);
 app.listen(3000);
+
+function transformJSON(req) {
+  var map = {
+    item: {
+        FirstName :   "properties.FirstName",
+        MiddleName :   "properties.MiddleName",
+        LastName :   "properties.LastName",
+        Nationality :   "properties.Nationality",
+        Email :   "",
+        Sex :   "properties.Sex",
+        Age :  "properties.ageNumber",
+        Region :   "properties.region",
+        SubcityOrZone :   "properties.subcity",
+        Woreda :   "properties.woreda",
+        Kebele :   "properties.Kebele",
+        HouseNo :   "properties.HouseNo",
+        PhoneNo :   "properties.PhoneNo",
+        Area :   "",
+        Occupation :   "",
+        CallDate :   "",
+        CallerType :   "",
+        TravelerId :   "",
+        Fever :   "properties.Fever",
+        FeverStartDate :   "properties.FeverStartDate",
+        Cough :   "properties.Cough",
+        CoughStartDate :   "properties.CoughStartDate",
+        Headache :  "properties.Headache",
+        HeadacheStartDate :   "properties.HeadacheStartDate",
+        SoreThroat :  "properties.SoreThroat",
+        SoreThroatStartDate :   "properties.SoreThroatStartDate",
+        RunnyNose :  " ",
+        RunnyNoseStartDate :   "",
+        UnwellnessFeeling :  "properties.UnwellnessFeeling",
+        UnwellnessFeelingStartDate :   "properties.UnwellnessFeelingStartDate",
+        BreathingDifficulty :  "properties.BreathingDifficulty",
+        BreathingDifficultyStartDate :   "properties.BreathingDifficultyStartDate",
+        BodyPain :  "properties.BodyPain",
+        BodyPainStartDate :   "properties.BodyPainStartDate",
+        TravleHx :  "properties.TravleHx",
+        HaveSex :  "",
+        AnimalMarket :  "",
+        HealthFacility :  "true",
+        ReceiverName :   "",
+        CheckedBy :   "",
+        Remark :    "properties.remark",
+        HouseToHouseID :   "",
+        Source :   "properties.case_type",
+        FormStatus :   ""
+
+    }
+  };
+
+  const resultObj = transform(req.body, map);
+  return resultObj;
+}
